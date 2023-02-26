a53ef9c6a21a37542ae10840f319bd638e2730fb
Stanislau Hekht <hekhtstanislau@gmail.com>
2023-02-26 14:59:55 -0500

    Begin implementing error and exception handling

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
+const { errorHandler } = require('../backend/middleware/errorMiddleware');
 const PORT = process.env.PORT || 5000;
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
   res.status(200).json({ message: 'Welcome to the Support Desk API' });
@@ -9,5 +13,7 @@ app.get('/', (req, res) => {
 
 // Routes
 app.use('/api/users', require('./routes/userRoutes'));
+
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
